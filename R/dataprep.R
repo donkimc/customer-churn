815f373637b9f821e5f54f8a18cf9136aa1032e5
Don Kim <dkim05@gmail.com>
2019-11-13 18:39:59 -0500

    Copy code from guidelines to get summary

--- R/dataprep.R
+++ R/dataprep.R
@@ -1,4 +1,8 @@
 #file.exists("R/churn.csv")
 #getwd()
-ds <- read.csv("R/churn.csv", header = TRUE)
-summary(ds$Account.Length)
+#ds <- read.csv("R/churn.csv", header = TRUE)
+#summary(ds$Account.Length)
+gc <- read.csv("R/churn.csv",header = TRUE, stringsAsFactors = FALSE,
+               na.string=c("","NA"))
+str(gc)
+summary(gc)
