8d3f313862a040a405b34644499ca6c1ff526c73
Don Kim <dkim05@gmail.com>
2019-11-16 16:53:52 -0500

    fix minor bug

--- R/dataprep.R
+++ R/dataprep.R
@@ -15,7 +15,7 @@ boxplot(gc$VMail.Message)
 boxplot(gc$Day.Mins)
 boxplot(gc$Day.Calls)
 
-#z score
+#z score and outlier remove
 z <- scale(gc$Account.Length,center=TRUE, scale=TRUE)
 out_right <- which(z>2)
 out_left <- which(z<(-2))
@@ -28,28 +28,31 @@ gc$Account.Length[out_left] <- m
 outl_remove <- function(gcc) {
   x <- c(2,7:20)
   for (i in x) {
-    z <- scale(gc[i],center=TRUE, scale=TRUE)
-    m <- median(gc[i])
-    gc[i,which(z>2)] <- m #right
-    gc[i,which(z<(-2))] <- m #left
+    z <- scale(gc[,i],center=TRUE, scale=TRUE)
+    m <- median(gc[,i])
+    gcc[which(z>2),i] <- m #right
+    gcc[which(z<(-2)),i] <- m #left
   }
+  gcc
 }
 
+#draw multiple boxplots
 plot_boxplots <- function(gcc) {
   x <- c(2,7:20)
   par(mfrow=c(4,4))
   for (i in x) {
-    boxplot(gc[i],main=names(gc[i]))
+    boxplot(gcc[i],main=names(gcc[i]))
   }
 }
 plot_boxplots(gc)
 
-#boxplot(gc$Account.Length)
+#outlier remove using boxplot and IQC
 ds <- read.csv("R/churn.csv", header = TRUE)
 summary(ds$Account.Length)
 outlier<-boxplot(ds$Account.Length)$out
 newds<-ds[-c(which(ds$Account.Length==outlier)),]
 
+#rewrite to csv file
 
 
 #correlation
